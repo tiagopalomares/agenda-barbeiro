e62fe084ad02ac918727d3f21e7b9d49c325fec6
Tiago Palomares <tiagopalomares@gmail.com>
2025-07-09 11:48:45 -0300

    Adicionado entidade Agendamento e ajustada estrutura de pacotes

--- src/main/java/com/barbearia/agenda_barbeiro/model/Agendamento.java
+++ src/main/java/com/barbearia/agenda_barbeiro/model/Agendamento.java
@@ -1,10 +1,13 @@
-package com.barbearia.agendabarbeiro.model;
+package com.barbearia.agenda_barbeiro.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data; 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
