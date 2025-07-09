c44bc9c0f1d852b844a5753789becee6d48f14ff
Tiago Palomares <tiagopalomares@gmail.com>
2025-07-09 10:17:29 -0300

    Primeiro Commit: Configuração inicial do projeto com  Spring Initializr

---
+++ agenda-barbeiro/src/main/java/com/barbearia/agenda_barbeiro/AgendaBarbeiroApplication.java
@@ -0,0 +1,13 @@
+package com.barbearia.agenda_barbeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaBarbeiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaBarbeiroApplication.class, args);
+	}
+
+}
---
+++ agenda-barbeiro/src/test/java/com/barbearia/agenda_barbeiro/AgendaBarbeiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbearia.agenda_barbeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaBarbeiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
